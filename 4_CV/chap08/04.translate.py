7e72a05c7e98e49128cb84a43f677db3af29ff20
Hyubbbb <ekwjsxl200@gmail.com>
2025-01-22 17:38:22 +0900

    250122

--- 4_CV/chap08/04.translate.py
+++ 4_CV/chap08/04.translate.py
@@ -1,23 +1,29 @@
 import numpy as np, cv2
 
 def contain(p, shape):                              # 좌표(y,x)가 범위내 인지 검사
-    return 
+    return 0 <= p[0] < shape[0] and 0 <= p[1] < shape[1]
 
 def translate(img, pt):
-   
-
-
-
-
+    dst = np.zeros(img.shape, img.dtype)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            x, y = np.subtract((j, i), pt)
+            if contain((y, x), img.shape):
+                dst[i, j] = img[y, x]
     return dst
 
-image = cv2.imread('Source/Chap08/images/translate.jpg', cv2.IMREAD_GRAYSCALE)
+image = cv2.imread('4_CV/chap08/images/translate.jpg', cv2.IMREAD_GRAYSCALE)
 if image is None: raise Exception("영상 파일을 읽기 에러")
 
-dst1 = 
-dst2 = 
+x_translation = int(input("x translation: "))
+y_translation = int(input("y translation: "))
+
+# dst1 = translate(image, (30, 80))
+# dst2 = translate(image, (-70, -50))
+dst3 = translate(image, (x_translation, y_translation))
 
 cv2.imshow("image", image)
-cv2.imshow("dst1: trans to (80, 30)", dst1);
-cv2.imshow("dst2: trans to (-50, -70)", dst2);
+# cv2.imshow("dst1: trans to (30, 80)", dst1);
+# cv2.imshow("dst2: trans to (-70, -50)", dst2);
+cv2.imshow(f"dst3: trans to ({x_translation}, {y_translation})", dst3);
 cv2.waitKey(0)
